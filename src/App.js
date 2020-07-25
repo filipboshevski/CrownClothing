44f809692f7477937c9a42f60d2697953cce95d0
filipboshevski <filipbosevski1234@gmail.com>
2020-07-25 20:03:59 +0200

    Homepage complete design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import './Components/Homepage Component/Homepage Component';
-import HomePage from './Components/Homepage Component/Homepage Component';
+import HomePage from './components/Homepage/HomePage';
 
 function App() {
   return (
