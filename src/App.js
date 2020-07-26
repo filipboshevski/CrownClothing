9d04fabfb117d96af7f01beef9ae40b0fbe4325a
filipboshevski <filipbosevski1234@gmail.com>
2020-07-26 17:09:45 +0200

    Hats Page Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/Homepage/HomePage';
+import { Route } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact component={HomePage} path='/'/>
+      <Route component={HatsPage} path='/hats'/>
     </div>
   );
 }
