c5ce2d8e69932e923654addb9a130c20435e3d6a
filipboshevski <filipbosevski1234@gmail.com>
2020-09-04 15:24:35 +0200

    Fixing a console log from collection component.

--- src/components/pages/Collection/Collection.js
+++ src/components/pages/Collection/Collection.js
@@ -7,7 +7,6 @@ import CollectionItem from '../../collection-item/CollectionItem';
 
 const Collection = ({ collection }) => {
     const { title, items } = collection;
-    console.log(collection);
     return (
         <div className='collection-page'>
             <h2 className='title'>{title}</h2>
