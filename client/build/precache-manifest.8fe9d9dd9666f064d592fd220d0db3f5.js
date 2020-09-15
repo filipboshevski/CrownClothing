4e54978290343bf10cd739f7df51521e1a5bdbcf
filipboshevski <filipbosevski1234@gmail.com>
2020-09-15 13:57:27 +0200

    Making a build

---
+++ client/build/precache-manifest.8fe9d9dd9666f064d592fd220d0db3f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5de83c3f8414e083fd9edee3fa3bfd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e06ca8c8a1ec9e12923f",
+    "url": "/static/css/main.501ab51f.chunk.css"
+  },
+  {
+    "revision": "80d34577bc8f408ea606",
+    "url": "/static/js/2.516316d3.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.516316d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06ca8c8a1ec9e12923f",
+    "url": "/static/js/main.1e5aa9e8.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
